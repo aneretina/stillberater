e19c7cee6d8b3c956eb747a8c4a8c0dcc45f91f1
Anna Neretina <anna.neretina@crowdcode.io>
2025-02-24 12:36:14 +0100

    refactoring

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,22 @@
-import { defineConfig, squooshImageService } from 'astro/config';
-import viteSassGlob from 'vite-plugin-sass-glob-import';
-import icon from 'astro-icon';
+import { defineConfig, squooshImageService } from 'astro/config'
+import viteSassGlob from 'vite-plugin-sass-glob-import'
+import icon from 'astro-icon'
 
-// const outputPluginStats = () => ({
-//   name: 'output-plugin-stats',
-//   configResolved(config) {
-//     const plugins = config.plugins.map((plugin) => plugin.name)
-//     console.log(`Your project has ${plugins.length} Vite plugins.`)
-//     console.table(plugins)
-//   }
-// }) // показывает все подключенные плагины
-
-// const noAttr = () => {
-//   return ({
-//     name: 'no-attribute',
-//     enforce: 'post',
-//     transformIndexHtml(html) {
-//       return html.replace('type="module"', '')
-//     },
-//   })
-// } // должен убирать type="module" из тега скрипт
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/',
   devToolbar: {
     enabled: false
   },
-  site: 'https://htmlonelove.github.io',
   compressHTML: false,
   output: 'static',
   publicDir: './public',
   build: {
     format: 'file',
-    // вытаскивает вложенные страницы в корень src/pages/subpage/subpage.html => dist/subpage.html
     assets: 'assets',
-    // собирает скрипты и стили в папку dist/assets
-    assetsPrefix: '.' // добавляет `.` в пути скриптов и стилей
-    // inlineStylesheets: 'never', // запрещает инлайн стилей
+    assetsPrefix: '.'
   },
   image: {
     service: squooshImageService()
@@ -51,19 +29,17 @@ export default defineConfig({
   vite: {
     build: {
       assetsInlineLimit: 0,
-      // запрещает инлайн скриптов. по дефолту инлайнит скрипты в html
       cssCodeSplit: false,
-      // css в один файл
       rollupOptions: {
         output: {
           entryFileNames: 'scripts.js',
-          assetFileNames: assetInfo => {
-            return assetInfo.name === 'style.css' ? `${assetInfo.name}` // задается имя и папка (корень) для css
-            : `assets/${assetInfo.name}`; // задается имя и папка картинкам
+          assetFileNames: (assetInfo) => {
+            return assetInfo.name === 'style.css' ? `${assetInfo.name}`
+              : `assets/${assetInfo.name}`
           }
         }
       }
     },
     plugins: [viteSassGlob()]
   }
-});
+})
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,16 @@
 import { initModals } from './modules/modals/init-modals'
 import { mobileVhFix } from './utils/mobile-vh-fix'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 mobileVhFix()
 document.addEventListener(
   'DOMContentLoaded',
   () => {
     initModals()
+    AOS.init()
   },
   true
 )
+
+
