92ad7a4d08ab95744ce72e991660b3e8b796390c
Anna Neretina <anna.neretina@crowdcode.io>
2025-02-24 13:00:58 +0100

    test

--- .history/src/scripts/index_20250224121707.js
+++ .history/src/scripts/index_20250224121707.js
@@ -1,14 +1,14 @@
 import { initModals } from './modules/modals/init-modals'
 import { mobileVhFix } from './utils/mobile-vh-fix'
-// import AOS from 'aos'
-// import 'aos/dist/aos.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 mobileVhFix()
 document.addEventListener(
   'DOMContentLoaded',
   () => {
     initModals()
-    // AOS.init()
+    AOS.init()
   },
   true
 )
